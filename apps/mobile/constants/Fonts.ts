899631f94a58115461aa84569f737e69ac0f24e4
Rodrigoatila09 <154346800+Rodrigoatila09@users.noreply.github.com>
2025-10-18 11:24:33 -0300

    feat: Adicona arquivo para as fontes do Aton

---
+++ apps/mobile/constants/Fonts.ts
@@ -0,0 +1,13 @@
+export const Fonts = {
+
+    primaryFont:{
+        dongleLight:"Dongle-Light",
+        dongleRegular:"Dongle-Regular",
+        dongleBold:"Dongle-Bold"
+    },
+        
+    size:{
+
+    }
+
+}
