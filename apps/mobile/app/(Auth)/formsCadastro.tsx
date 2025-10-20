48339dd6e8fb2dc7bf616f68bd6ec37202c0dd4a
Rodrigoatila09 <154346800+Rodrigoatila09@users.noreply.github.com>
2025-10-19 23:32:21 -0300

    feat (Cadastro - Etapa 2): Cria segunda tela do Cadastro

---
+++ apps/mobile/app/(Auth)/formsCadastro.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View, Image, TextInput } from "react-native";
+import React from "react";
+import NamedLogo from "../../assets/img/Logo_1_Atom.png";
+import Button1Comp from "../../components/Button1Comp";
+import { Colors } from "../../constants/Colors";
+import { useRouter } from "expo-router";
+
+
+const FormsCadastro: React.FC = () => {
+    return <FormsCadastroInner></FormsCadastroInner>
+};
+
+const FormsCadastroInner : React.FC = () => {
+    return 
+
+}
