07206641acf21c4918a6b66ba9525f2043200df9
Rodrigoatila09 <154346800+Rodrigoatila09@users.noreply.github.com>
2025-10-20 18:21:16 -0300

    feat(Cadastro 2): Cria estrura base para a segunda etapa do formulário

--- apps/mobile/app/(Auth)/formsCadastro.tsx
+++ apps/mobile/app/(Auth)/formsCadastro.tsx
@@ -1,16 +1,81 @@
-import { StyleSheet, Text, View, Image, TextInput } from "react-native";
+import { StyleSheet, Text, View, Image, TextInput, ScrollView, TouchableOpacity } from "react-native";
 import React from "react";
-import NamedLogo from "../../assets/img/Logo_1_Atom.png";
-import Button1Comp from "../../components/Button1Comp";
-import { Colors } from "../../constants/Colors";
-import { useRouter } from "expo-router";
+import { router, Router, useRouter } from "expo-router";
+import Button1Comp from "@/components/Button1Comp";
+import { Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import Logo from "../../assets/img/Logo_1_Atom.png";
 
-
-const FormsCadastro: React.FC = () => {
-    return <FormsCadastroInner></FormsCadastroInner>
+const FormsCadastro: React.FC = () => {     
+    return <FormsCadastroInner/>
 };
 
 const FormsCadastroInner : React.FC = () => {
-    return 
+        const router = useRouter();
+
+        const comebackPage = () => {
+            router.push("/(Auth)/cadastro");
+        }
+
+    return (
+
+        <ScrollView style={styles.scrollContainer}>
+             <View style={styles.container}>
+
+                <View style={styles.backPageDiv}>
+                    <TouchableOpacity onPress={comebackPage}>
+                        <Ionicons name="arrow-back" color={Colors.light.gray} size={35}/>
+                    </TouchableOpacity>
+
+                </View>
+                
+                <View style={styles.imageDiv}>
+                    <Image source={Logo}/>
+                </View>
+                
+                <View style={styles.containerInfos}>
+                    <View style={styles.txtDiv}></View>
+                </View>
+
+             
+             </View>
+
+
+
+        </ScrollView>
+
+    )
 
 }
+
+const styles = StyleSheet.create({
+    scrollContainer:{
+        flex:1,
+        width:'100%'
+    },
+
+    container:{
+        width:'100%'
+    },
+    backPageDiv:{
+        height:100,
+        width:"100%",
+    },
+    imageDiv:{
+        height:200,
+        width:'100%',
+        backgroundColor:'green',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    containerInfos:{
+        height:573,
+        width:'100%',
+        backgroundColor:'blue'
+    },
+    txtDiv:{
+
+    }
+
+})
+export default FormsCadastro;
