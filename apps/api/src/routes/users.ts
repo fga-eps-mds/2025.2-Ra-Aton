d3f7a57a8d1be112a2ccd48d9f699189468286ad
yanrdgs-dev <yansantosrodrigues.dev@gmail.com>
2025-10-23 02:00:18 -0300

    cleanup: removendo comentários de import
    Co-authored-by: Gabriel Alves <g.alvesdearaujo06@gmail.com>

--- apps/api/src/routes/users.ts
+++ apps/api/src/routes/users.ts
@@ -6,7 +6,6 @@ import {
   updateUser,
   deleteUser,
 } from '../controllers/usersController';
-//import { authUser } from '../auth/auth.routes';
 const router: Router = Router();
 
 router.get('/', listUsers); // SELECT *
