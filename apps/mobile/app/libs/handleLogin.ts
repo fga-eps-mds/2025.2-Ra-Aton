f128ce7a833c29bcbd89cad5154834157d17a4c7
Samuel Leite <samuels.miner@gmail.com>
2025-10-25 11:53:05 -0300

    estrutura de chamadas de API

---
+++ apps/mobile/app/libs/handleLogin.ts
@@ -0,0 +1,28 @@
+async function handleLogin(email: string, password: string) {
+  const response = await fetch("http://localhost:4000/api/login", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    console.log("Mensagem de erro: " + data.message);
+    throw new Error(data.message || "Login failed");
+  }
+  console.log("Login successful:", data);
+  return data;
+}
+
+/*
+body = {
+    email: "email",
+    password: "senha"
+}
+const data = {
+    response: 200, // deu certo
+    token: fnkfjs137418fjk
+}
+
+*/
