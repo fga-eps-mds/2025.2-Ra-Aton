714d2cd82bd0d8e8ef3eccd2da7d0485950a6734
matheus-06 <matheus2006.pinheiro@gmail.com>
2025-10-27 15:36:53 -0300

    feat(SecondaryButton): Possibilidade de adicionar estilização de texto no botao secundário issue: #11
    Co-authored-by: Rodrigo <rodrigoatila09@gmail.com>

--- apps/mobile/components/SecondaryButton.tsx
+++ apps/mobile/components/SecondaryButton.tsx
@@ -5,16 +5,19 @@ import {
   ViewStyle,
   TouchableOpacity,
   TouchableOpacityProps,
+  TextStyle,
 } from "react-native";
 import { useTheme } from "@/constants/Theme";
 import { Colors } from "@/constants/Colors";
 type Button2CompProps = {
   style?: StyleProp<ViewStyle>;
+  decoration?: StyleProp<TextStyle>;
 } & TouchableOpacityProps;
 
 const Button2Comp: React.FC<Button2CompProps> = ({
   style,
   children,
+  decoration,
   ...props
 }) => {
   const { isDarkMode } = useTheme();
@@ -39,12 +42,14 @@ const Button2Comp: React.FC<Button2CompProps> = ({
       {...props}
     >
       <Text
-        style={{
+        style={[{
           color: theme.text,
           textAlign: "center",
           fontSize: 16,
           fontWeight: "500",
-        }}
+        },
+      decoration,
+    ]}
       >
         {children}
       </Text>
