83f16ad1ab26742beaef51b8b97f33d7ef611105
matheus-06 <matheus2006.pinheiro@gmail.com>
2025-11-01 12:01:50 -0300

    feat(index): A página inicial agora é baseada no profileType issue: #51

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
-import { Redirect } from "expo-router";
-import * as SecureStore from "expo-secure-store";
+import { Redirect, useRouter } from "expo-router";
 import { ActivityIndicator, View } from "react-native";
 import { getToken } from "@/libs/storage/getToken";
 import { getUserData } from "@/libs/storage/getUserData";
 import { Colors } from "@/constants/Colors";
 import { useTheme } from "@/constants/Theme";
+
 export default function Index() {
   const [isLoading, setIsLoading] = useState(true);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const { isDarkMode } = useTheme();
+const [tela, setTela] = useState<string | null>(null);
+  const { isDarkMode } = useTheme();
   const theme = isDarkMode ? Colors.dark : Colors.light;
 
   useEffect(() => {
@@ -18,30 +18,49 @@ export default function Index() {
         const token = await getToken();
         if (token) {
           const userData = await getUserData();
-          console.log("Usuário logado:", userData);
-          setIsLoggedIn(true);
+        
+          switch (userData.profileType) {
+            case "JOGADOR":
+            case "TORCEDOR":
+            case "ATLETICA":
+              setTela("Teams");
+              break;
+            default:
+              setTela("Home");
+          }
+        } else {
+          setTela("login");
         }
       } catch (error) {
         console.log("Erro ao verificar token:", error);
+        setTela("cadastro");
       } finally {
         setIsLoading(false);
       }
     };
 
     checkAuth();
-  }, []); // O " [] " faz a função rodar 1x e não toda renderização
+  }, []);   // [] é pra rodar só 1x
 
   if (isLoading) {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor:theme.background }}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: theme.background,
+        }}
+      >
         <ActivityIndicator size="large" color={theme.orange} />
       </View>
     );
   }
 
-  if (isLoggedIn) {
-    return <Redirect href="/(DashBoard)/Home" />; // deveria depender do userType
+  // Só faz o redirecionamento quando o estado estiver definido
+  if (tela) {
+    return <Redirect href={`http://localhost:8081/${tela}`} />;
   }
 
-  return <Redirect href="/(Auth)/cadastro" />;
+  return null;
 }
