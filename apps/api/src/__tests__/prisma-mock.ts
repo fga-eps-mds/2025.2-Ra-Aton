8f1f3590c65f002aa5246ad326e13a40c9071b85
Gabriel <g.alvesdearaujo06@gmail.com>
2025-11-02 16:26:05 -0300

    test(mock_config): configurando mock geral do prisma.

--- apps/api/src/__tests__/prisma-mock.ts
+++ apps/api/src/__tests__/prisma-mock.ts
@@ -2,11 +2,15 @@
 // Você precisará instalar: pnpm add -D jest-mock-extended -w
 
 import { PrismaClient } from "@prisma/client";
-import { mockDeep, DeepMockProxy } from "jest-mock-extended";
+import { mockDeep, mockReset, DeepMockProxy } from "jest-mock-extended";
 
 // Tipagem para o nosso mock
 export type DeepMockPrisma = DeepMockProxy<PrismaClient>;
 
 // Cria e exporta o mock singleton
-// O 'as unknown as DeepMockPrisma' é um truque de tipagem necessário
-export const prismaMock = mockDeep<PrismaClient>() as unknown as DeepMockPrisma;
+export const prismaMock: DeepMockPrisma = mockDeep<PrismaClient>();
+
+// Garante que os mocks sejam limpos entre testes
+beforeEach(() => {
+  mockReset(prismaMock);
+});
