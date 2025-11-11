934f46c3fd66fe54776b2cd6253e74a9e72fe23c
Samuel Leite <samuels.miner@gmail.com>
2025-11-11 15:16:10 -0300

    Minha alteração na api auth

--- apps/mobile/libs/auth/api.tsx
+++ apps/mobile/libs/auth/api.tsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const api_route = axios.create({
     // Coloque seu IP aqui
-    baseURL: "http://XXX.XXX.XXX.XXX:4000",
+    baseURL: "http://XXX.XXX.XXX:4000",
     timeout: 5000
 })
 // Coloque seu IP aqui
-export const IP: string = "http://XXX.XXX.XXX.XXX:4000";
+export const IP: string = "http://XXX.XXX.XXX:4000";
