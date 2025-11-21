49176009e1fb82feb45bbc991515d54b7ccbf030
Rodrigoatila09 <154346800+Rodrigoatila09@users.noreply.github.com>
2025-11-21 20:52:33 -0300

    refactor(Feed de partidas) : Adiciona ao feed de partidas os componentes que serão utilizados
    #59
    
    Co-authered-by: Rodrigo <rodrigoatila09@gmail>

--- apps/mobile/app/(DashBoard)/(tabs)/Partidas.tsx
+++ apps/mobile/app/(DashBoard)/(tabs)/Partidas.tsx
@@ -7,13 +7,18 @@ import SearchInputComp from "@/components/SearchInputComp";
 import InputComp from "@/components/InputComp";
 import BackGroundComp from "@/components/BackGroundComp";
 import { MatchesCard } from "@/components/MatchesCardComp";
+import { HandleMatchComp } from "@/components/HandleMatchComp";
+import { useModal } from "@/libs/hooks/useModal";
+import { EventInfoModalComp } from "@/components/EventInfoModal";
+import { EventDetailsModal } from "@/components/EventDetailsModal";
+
 
 export default function Partidas() {
   const { isDarkMode } = useTheme();
   const themeStyles = StyleSheet.create({
     container: {
       // flexDirection:'column',
-      alignItems:'center',
+      alignItems: 'center',
       justifyContent: "center",
       backgroundColor: isDarkMode
         ? Colors.dark.background
@@ -21,15 +26,23 @@ export default function Partidas() {
     },
   });
 
+  const {
+    visible, closeModal, openModal
+  } = useModal();
+ 
+
   return (
     <BackGroundComp>
       <View style={styles.container}>
-      
 
-    <MatchesCard></MatchesCard>
+        <MatchesCard onPressInfos={openModal}></MatchesCard>
+        <EventDetailsModal visible={visible} onClose={closeModal}></EventDetailsModal>
+{/* 
+        <HandleMatchComp
+          onClose={closeModal}
+        ></HandleMatchComp> */}
 
       </View>
-
 
 
     </BackGroundComp>
@@ -37,12 +50,17 @@ export default function Partidas() {
 }
 
 const styles = StyleSheet.create({
-  container:{
-    height:'100%',
-    width:'100%',
-    flexDirection:'column',
-    alignItems:'center',
-    justifyContent:'center',
+  container: {
+    height: '100%',
+    width: '100%',  
+    flexDirection: "column",
+    alignItems:"center",
+    justifyContent:"center",
+    paddingHorizontal: 15,
+    paddingTop: 8,
+    paddingBottom: 8,
+    columnGap: 10,
+    // justifyContent:'cent er',
 
   }
 })
