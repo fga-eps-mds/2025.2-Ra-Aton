3a54d25f2e945ffd42c3d6b9b018984c761ed416
Gabriel <gabriel1990mota@gmail.com>
2025-11-27 19:32:40 -0300

    fix(tests): correção dos testes de criação do service de partidas para suportar scores e sports
    issue associada #25
    
    Co-authored-by: Gabriel <gabriel1990mota@gmail.com>

--- apps/api/src/__tests__/match/service/match.service.createMatch.test.ts
+++ apps/api/src/__tests__/match/service/match.service.createMatch.test.ts
@@ -3,6 +3,7 @@ import matchRepository from "../../../modules/match/match.repository";
 import { ApiError } from "../../../utils/ApiError";
 import httpStatus from "http-status";
 import { MatchStatus } from "@prisma/client";
+import { mock } from "jest-mock-extended";
 
 jest.mock("../../../modules/match/match.repository");
 
@@ -20,9 +21,12 @@ describe("MatchService.createMatch", () => {
       date: new Date(),
       maxPlayers: 4,
       location: "Local de Teste",
+      sport: "futsal",
       description: "Descrição de Teste",
       teamNameA: "Time A",
-      teamNameB: "Time B",
+      teamAScore: 3,
+      teamNameB: "Updatable Team B",
+      teamBScore: 2,
       author: {
         id: "1",
       },
@@ -31,19 +35,22 @@ describe("MatchService.createMatch", () => {
     const repo = jest.mocked(matchRepository);
 
     const createdMatch = {
-        MatchDate: mockData.date,
-        id: "m_1",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        title: mockData.title,
-        description: mockData.description,
-        teamNameA: mockData.teamNameA,
-        teamNameB: mockData.teamNameB,
-        authorId: mockData.author.id,
-        maxPlayers: mockData.maxPlayers,
-        MatchStatus: MatchStatus.EM_BREVE,
-        location: mockData.location,
-    }
+      MatchDate: mockData.date,
+      id: "m_1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      title: mockData.title,
+      description: mockData.description,
+      teamNameA: mockData.teamNameA,
+      teamAScore: mockData.teamAScore,
+      teamNameB: mockData.teamNameB,
+      teamBScore: mockData.teamBScore,
+      authorId: mockData.author.id,
+      maxPlayers: mockData.maxPlayers,
+      MatchStatus: MatchStatus.EM_BREVE,
+      location: mockData.location,
+      sport: mockData.sport,
+    };
 
     repo.createMatch.mockResolvedValue(createdMatch);
 
@@ -54,7 +61,7 @@ describe("MatchService.createMatch", () => {
     expect(result).toBeDefined();
     expect(repo.createMatch).toHaveBeenCalledWith(mockData, mockData.author);
     expect(repo.createMatch).toHaveBeenCalledTimes(1);
-    expect(result).toMatchObject(createdMatch)
+    expect(result).toMatchObject(createdMatch);
   });
 
   it("deve lancar um erro se o autor nao for passado na partida", async () => {
@@ -62,9 +69,8 @@ describe("MatchService.createMatch", () => {
     const mockData = {
       title: "Partida de Teste",
       date: new Date(),
-      maxPlayers: 4
+      maxPlayers: 4,
     };
-
 
     // 2. ACT & ASSERT
     await expect(matchService.createMatch(mockData)).rejects.toThrow(
