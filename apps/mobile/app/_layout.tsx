e81cf5e498bf20b0952bbf5f885295b6ca293e9d
Gabriel <g.alvesdearaujo06@gmail.com>
2025-11-29 04:59:43 -0300

    fix: resolve erro do frontend e migrantions antigas

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -53,7 +53,20 @@ export default function RootLayout() {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <UserProvider>
-          <AppContent />
+          <NotificationProvider>
+            {/* A Stack deve envolver TUDO o que é navegação */}
+            <Stack
+              screenOptions={{
+                headerShown: false, // Padrão: sem header
+              }}
+            >
+              {/* Rotas principais */}
+              <Stack.Screen name="index" />
+              <Stack.Screen name="(Auth)" />
+              <Stack.Screen name="(DashBoard)" />
+              <Stack.Screen name="perfilGrupo" options={{ presentation: 'card' }} />
+            </Stack>
+          </NotificationProvider>
         </UserProvider>
       </ThemeProvider>
     </QueryClientProvider>
