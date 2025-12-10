55d9a8eb47fe7d3aa9d867cdbde6248a04714b9a
Gabriel <gabriel1990mota@gmail.com>
2025-12-10 14:46:07 -0300

    feat(Avaliações): finalização da criação da logica de criação de listagem, criação, atualização e exclusão de avaliações
    issue associada: #137
    
    Co-authored-by: Gabriel <gabriel1990mota@gmail.com>

---
+++ apps/api/src/modules/avaliation/avaliation.controller.ts
@@ -0,0 +1,79 @@
+import { Request, Response } from "express";
+import avaliationService from "./avaliation.service";
+import HttpStatus from "http-status";
+import { ApiError } from "../../utils/ApiError";
+
+class AvaliationController {
+    async findAllAvals(req: Request, res: Response) {
+        const avals = await avaliationService.findAllAvals();
+        return res.status(HttpStatus.OK).json(avals);
+    }
+
+    async findAllAvalsByUserId(req: Request, res: Response) {
+        const authUser = (req as any).user;
+        if (!authUser || !authUser.id) {
+            return res.status(HttpStatus.UNAUTHORIZED).json({ message: "Usuário não está autenticado" })
+        }
+
+        const avals = await avaliationService.findAllAvalsByUserId(authUser.id);
+        return res.status(HttpStatus.OK).json(avals);;
+    }
+
+    async findAvalById(req: Request, res: Response) {
+        const { id } = req.params;
+        if (!id) {
+            return res.status(HttpStatus.BAD_REQUEST).json({ message: "O id da avaliação não foi recebido corretamente" })
+        }
+
+        const aval = avaliationService.findAvalById(id);
+        return res.status(HttpStatus.FOUND).json(aval);
+    }
+
+    async createAval(req: Request, res: Response) {
+        const authUser = (req as any).user;
+        if (!authUser || !authUser.id) {
+            return res.status(HttpStatus.UNAUTHORIZED).json({ message: "Usuário não está autenticado" })
+        }
+
+        const data = req.body;
+        const newAval = avaliationService.createAval(data, authUser.Id);
+        return res.status(HttpStatus.CREATED).json(newAval);
+    }
+
+    async updateAval(req: Request, res: Response) {
+        const { id } = req.params;
+        const data = req.body;
+        if (!id) {
+            return res.status(HttpStatus.BAD_REQUEST).json({ message: "O id da avaliação não foi recebido corretamente" })
+        }
+
+        try {
+            const updatedAval = avaliationService.updateAval(data, id);
+            return res.status(HttpStatus.OK).json(updatedAval);
+        } catch (error) {
+            if (error instanceof ApiError) {
+                return res.status(error.statusCode).json({ message: error.message })
+            }
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: "Erro interno do servidor" })
+        }
+    }
+
+    async deleteAval(req: Request, res: Response) {
+        const { id } = req.params;
+        if (!id) {
+            return res.status(HttpStatus.BAD_REQUEST).json({ message: "O id da avaliação não foi recebido corretamente" })
+        }
+
+        try {
+            await avaliationService.deleteAval(id);
+            return res.status(HttpStatus.NO_CONTENT).send();
+        } catch (error) {
+            if (error instanceof ApiError) {
+                return res.status(error.statusCode).json({ message: error.message })
+            }
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: "Erro interno do servidor" })
+        }
+    }
+}
+
+export default new AvaliationController();
