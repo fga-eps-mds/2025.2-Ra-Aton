436753fc330e457d53d4044876f671d873bae100
Gabriel <gabriel1990mota@gmail.com>
2025-12-10 13:10:07 -0300

    feat(Avaliações): criação da logica de criação de listagem, criação, atualização e exclusão de avaliações (service e repository)
    issue associada: #137
    
    Co-authored-by: Gabriel <gabriel1990mota@gmail.com>

---
+++ apps/api/src/modules/avaliation/avaliation.repository.ts
@@ -0,0 +1,73 @@
+import { prisma } from "../../database/prisma.client"
+import { Prisma, Avaliation } from "@prisma/client"
+
+class AvaliationRepository {
+    async findAllAvals(): Promise<Avaliation[]> {
+        return prisma.avaliation.findMany({
+            orderBy: { createdAt: "desc" },
+            include: {
+                user: {
+                    select: { id: true, userName: true, email: true },
+                }
+            }
+        })
+    }
+
+    async findAllAvalsByUserId(userId: string): Promise<Avaliation[]> {
+        return prisma.avaliation.findMany({
+            where: { id: userId },
+            orderBy: { createdAt: "desc" },
+            include: {
+                user: {
+                    select: { id: true, userName: true, email: true },
+                }
+            }
+        })
+    }
+
+    async findAvalById(id: string): Promise<Avaliation | null> {
+        return prisma.avaliation.findUnique({
+            where: { id },
+            include: {
+                user: {
+                    select: { id: true, userName: true, email: true },
+                }
+            }
+        })
+    }
+
+    async createAval(data: Prisma.AvaliationCreateInput, userId: string): Promise<Avaliation> {
+        return prisma.avaliation.create({
+            data: {
+                ...data,
+                user: {
+                    connect:
+                        { id: userId }
+                }
+            },
+            include: {
+                user: {
+                    select: { id: true, userName: true, email: true },
+                }
+            }
+        })
+    }
+
+    async updateAval(id: string, data: Prisma.AvaliationUpdateInput): Promise<Avaliation> {
+        return prisma.avaliation.update({
+            where: { id },
+            data,
+            include: {
+                user: {
+                    select: { id: true, userName: true, email: true },
+                }
+            }
+        })
+    }
+
+    async deleteAval(id: string): Promise<void> {
+        await prisma.avaliation.delete({ where: { id } })
+    }
+}
+
+export default new AvaliationRepository();
---
+++ apps/api/src/modules/avaliation/avaliation.service.ts
@@ -0,0 +1,67 @@
+import { Avaliation } from "@prisma/client"
+import AvaliationRepository from "./avaliation.repository"
+import { ApiError } from "../../utils/ApiError"
+import HttpStatus from "http-status"
+import avaliationRepository from "./avaliation.repository"
+
+class AvaliationService {
+    findAllAvals = async (): Promise<Avaliation[]> => {
+        const avals = await AvaliationRepository.findAllAvals();
+        return avals.map((aval) => {
+            return aval;
+        })
+    }
+
+    findAllAvalsByUserId = async (userId: string): Promise<Avaliation[]> => {
+        const avals = await AvaliationRepository.findAllAvalsByUserId(userId);
+        return avals.map((aval) => {
+            return aval;
+        })
+    }
+
+    findAvalById = async (id: string): Promise<Avaliation> => {
+        const aval = await AvaliationRepository.findAvalById(id);
+        if (!aval) {
+            throw new ApiError(
+                HttpStatus.NOT_FOUND,
+                "Avalição não foi encontrada"
+            )
+        }
+        return aval;
+    }
+
+    createAval = async (data: any, userId: string | undefined): Promise<Avaliation> => {
+        if (!userId) {
+            throw new ApiError(
+                HttpStatus.BAD_REQUEST,
+                "Id do usuário não foi recebido corretamente"
+            )
+        }
+
+        const newAval = AvaliationRepository.createAval(data, userId)
+        return newAval;
+    }
+
+    updateAval = async (data: any, id: string | null): Promise<Avaliation> => {
+        if (!id) {
+            throw new ApiError(
+                HttpStatus.BAD_REQUEST,
+                "O id da avaliação não for recebido corretamente"
+            )
+        }
+        const updatedAval = await avaliationRepository.updateAval(id, data);
+        return updatedAval;
+    }
+
+    deleteAval = async (id: string | null): Promise<void> => {
+        if (!id) {
+            throw new ApiError(
+                HttpStatus.BAD_REQUEST,
+                "O id da avaliação não for recebido corretamente"
+            )
+        }
+        await avaliationRepository.deleteAval(id);
+    } 
+}
+
+export default new AvaliationService();
