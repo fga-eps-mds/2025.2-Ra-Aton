30666183aca476a9d96b1924a9c7bfcea3310829
Gabriel <g.alvesdearaujo06@gmail.com>
2025-12-11 16:06:29 -0300

    fix(migrations): Corrige as novas migrations do prisma.

--- apps/api/prisma/migrations/20251210182324_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "bannerImageId" TEXT,
-ADD COLUMN     "bannerImageUrl" TEXT,
-ADD COLUMN     "profileImageId" TEXT,
-ADD COLUMN     "profileImageUrl" TEXT;
